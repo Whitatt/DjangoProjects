407d96e49d138653b14068540f34e582eeaba42a
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-09-23 18:59:27 -0700

    update

--- Django_Checkbook/Django_Checkbook/Checkbook/views.py
+++ Django_Checkbook/Django_Checkbook/Checkbook/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
 
 def home(request):
+    form = TransactionForm(data=request.POST or None)
+    if request.method == 'POST':
+        pk = request.POST['account']
+        return balance(request, pk)
+    content = {'form': form}
     return render(request, 'checkbook/index.html')
 
 def create_account(request):
@@ -11,9 +17,21 @@ def create_account(request):
             form.save()
             return redirect('index')
     content = {'form': form}
-    return render(request, 'checkbook/CreateNewAccount.html')
+    return render(request, 'checkbook/CreateNewAccount.html', content)
 
 def balance(request):
+    account = get_object_or_404(Account, pk=pk)
+    transactions = Transaction.Transactions.filter(account = pk)
+    current_total = account.initial_deposit
+    table_contents = {}
+    for t in transactions:
+        if t.type == 'Deposit':
+            current_total += t.amount
+            table_contents.update({t: current_total})
+        else:
+            current_total -= t.amount
+            table_contents.update({t : current_total })
+    content = {'account':account, 'table_contents': table_contents, 'balance' : current_total}
     return render(request, 'checkbook/BalanceSheet.html')
 
 def transaction(request):
@@ -21,6 +39,8 @@ def transaction(request):
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            return redirect('index')
+            pk = request.POST['account']
+            form.save()
+            return balance(request, pk)
     content = {'form': form}
-    return render(request, 'checkbook/AddTransaction.html')
+    return render(request, 'checkbook/AddTransaction.html', content)
